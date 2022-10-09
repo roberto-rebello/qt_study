5b4ba2db28fc363b62336cb0ce4d94954b6ee998
Roberto Rebello <roberto.rebello@gmail.com>
2022-10-08 21:19:56 -0300

    Adding menu demo to toolbar_demo

--- toolbar_demo.cpp
+++ toolbar_demo.cpp
@@ -8,6 +8,10 @@
 #include <QAction>
 #include <QStatusBar>
 
+#include <QMenuBar>
+
+#include <QApplication>
+
 ToolbarDemo::ToolbarDemo(QWidget *parent) : QMainWindow(parent)
 {
 
@@ -22,6 +26,7 @@ ToolbarDemo::ToolbarDemo(QWidget *parent) : QMainWindow(parent)
     label->setAlignment(Qt::AlignCenter);
     layout->addWidget(label);
 
+    // Toolbar Demo
     QToolBar *toolbar = new QToolBar("My Toolbar", this);
     toolbar->setIconSize(QSize(16, 16));
     addToolBar(toolbar);
@@ -37,8 +42,30 @@ ToolbarDemo::ToolbarDemo(QWidget *parent) : QMainWindow(parent)
     QLabel *label2 = new QLabel("Hey there!");
     toolbar->addWidget(label2);
 
+    QAction *action2 = new QAction(QIcon("images/icons/android.png"), "&Action 2", this);
+    action2->setStatusTip("This is another demo action");
+    action2->setCheckable(true);
+    connect(action2, SIGNAL(triggered(bool)), this, SLOT(action_triggered(bool)));
+    toolbar->addAction(action2);
+
     QStatusBar *statusBar = new QStatusBar(this);
     setStatusBar(statusBar);
+
+    // Menu Demo
+    QMenuBar *menu = menuBar();
+    QMenu *file_menu = menu->addMenu("&File");
+    file_menu->addAction(action);
+
+    QMenu *file_submenu = file_menu->addMenu("&Submenu");
+    file_submenu->addAction(action2);
+
+    file_menu->addSeparator();
+
+    QAction *exit_action = new QAction("&Exit", this);
+    exit_action->setStatusTip("Quit demo application");
+    exit_action->setShortcut(QKeySequence("ctrl+q"));
+    connect(exit_action, SIGNAL(triggered(bool)), qApp, SLOT(exit()));
+    file_menu->addAction(exit_action);
 }
 
 void ToolbarDemo::action_triggered(bool checked)
